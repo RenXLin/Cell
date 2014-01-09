37706975f72cf160945b9840d45ecba29a50c8a2
qianfeng1 <qianfeng1@RXL.local>
2014-01-09 10:31:56 +0800

    Initial Commit

---
+++ 自定义Cell/RXLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RXLAppDelegate.h
+//  自定义Cell
+//
+//  Created by qianfeng1 on 14-1-9.
+//  Copyright (c) 2014年 任小林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RXLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
